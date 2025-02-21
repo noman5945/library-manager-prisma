240f087605d5a5b6cc115282e3e161ed79ce7e91
noman5945 <abdullahnoman.5945@gmail.com>
2025-02-21 21:02:53 +0600

    [UPDATE] Refactored Request handler for simplicity

--- src/app/modules/Book/book.controller.ts
+++ src/app/modules/Book/book.controller.ts
@@ -2,26 +2,18 @@ import { Request, Response } from "express";
 import { bookServices } from "./book.service";
 import { BookType } from "./book.interface";
 import sendResponse from "../../utils/sendResponse";
+import requestHandler from "../../utils/requestHandler";
 
-const addNewBook = async (req: Request, res: Response) => {
+const addNewBook = requestHandler(async (req: Request, res: Response) => {
   const bookData: BookType = req.body;
-  try {
-    const result = await bookServices.addNewBook(bookData);
-    sendResponse(res, {
-      success: true,
-      statusCode: 200,
-      message: "Book created success fully!",
-      data: result,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: true,
-      statusCode: 200,
-      message: "Error! Something went wrong.",
-      error: error,
-    });
-  }
-};
+  const result = await bookServices.addNewBook(bookData);
+  sendResponse(res, {
+    success: true,
+    statusCode: 200,
+    message: "Book created success fully!",
+    data: result,
+  });
+});
 
 export const bookController = {
   addNewBook,
---
+++ src/app/utils/requestHandler.ts
@@ -0,0 +1,18 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+const requestHandler = (fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      res.status(500).json({
+        success: true,
+        statusCode: 500,
+        message: "Error! Something went wrong.",
+        error: error,
+      });
+    }
+  };
+};
+
+export default requestHandler;
